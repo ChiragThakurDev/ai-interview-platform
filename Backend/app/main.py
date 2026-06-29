7580b2ff534dba25a0b4a55635a40a3b2f4c5776
ChiragThakurDev <chirag212003@gmail.com>
2026-06-29 03:58:16 -0400

    Complete database setup with FastAPI and PostgreSQL

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,20 +1,34 @@
 from fastapi import FastAPI
-from app.core.config import settings
+from sqlalchemy import text
 
-app=FastAPI(
-        title=settings.app_name,
-        version=settings.app_version,
-        )
+from app.core.config import settings
+from app.db.session import engine
+
+app = FastAPI(
+    title=settings.app_name,
+    version=settings.app_version,
+)
+
 
 @app.get("/")
 async def root():
-    return{
-            "message": f"Welcome to {settings.app_name}"
-            }
+    return {
+        "message": f"Welcome to {settings.app_name}"
+    }
+
 
 @app.get("/health")
-async def health_check():
-    return{
-            "status":"healthy"
-            }
+def health_check():
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
 
+        return {
+            "status": "Database Connected Successfully"
+        }
+
+    except Exception as e:
+        return {
+            "status": "Database Connection Failed",
+            "error": str(e)
+        }
